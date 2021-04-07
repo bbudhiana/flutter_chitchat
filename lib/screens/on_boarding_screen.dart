ebe9a473c24167f207b07b17edb5f87f3f534ed6
= <=>
2021-04-08 01:42:43 +0700

    finish for on boarding

--- lib/screens/on_boarding_screen.dart
+++ lib/screens/on_boarding_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -12,6 +14,12 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
   final int _numPages = 3;
   final PageController _pageController = PageController(initialPage: 0);
   int _currentPage = 0;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   List<Widget> _buildPageIndicator() {
     List<Widget> list = [];
@@ -39,21 +47,15 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        systemNavigationBarDividerColor: Color(0xFF5B16D0),
-        systemNavigationBarColor: Color(0xFF5B16D0),
-        statusBarIconBrightness: Brightness.dark,
-        systemNavigationBarIconBrightness: Brightness.dark,
-      ),
-      child: Scaffold(
-        extendBodyBehindAppBar: true,
-        /* appBar: AppBar(
+    return Scaffold(
+      //extendBodyBehindAppBar: true,
+      /* appBar: AppBar(
           backgroundColor: Color(0xFF3594DD),
           title: Text('test'),
         ), */
-        body: Container(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
                 begin: Alignment.topCenter,
@@ -73,161 +75,216 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
           ),
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 40.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Container(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () => print('skip'),
-                      child: Text('Skip'),
-                      style: TextButton.styleFrom(
-                        primary: Colors.white,
-                        textStyle: TextStyle(
-                          fontSize: 20.0,
-                        ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () => print('skip'),
+                    child: Text('Skip'),
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      textStyle: TextStyle(
+                        fontSize: 20.0,
                       ),
                     ),
                   ),
-                  Container(
-                    height: 548,
-                    child: PageView(
-                      physics: ClampingScrollPhysics(),
-                      controller: _pageController,
-                      onPageChanged: (int page) {
-                        setState(() {
-                          _currentPage = page;
-                        });
-                      },
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(40.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Center(
-                                child: Image(
-                                  image:
-                                      AssetImage('assets/images/image_1.png'),
-                                  height: 300.0,
-                                  width: 300.0,
-                                ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.78,
+                  child: PageView(
+                    physics: ClampingScrollPhysics(),
+                    controller: _pageController,
+                    onPageChanged: (int page) {
+                      setState(() {
+                        _currentPage = page;
+                      });
+                    },
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                              child: Image(
+                                image: AssetImage('assets/images/image_1.png'),
+                                height: 300.0,
+                                width: 300.0,
                               ),
-                              SizedBox(
-                                height: 30.0,
+                            ),
+                            SizedBox(
+                              height: 30.0,
+                            ),
+                            Text(
+                              'Connect People\naround the world',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
                               ),
-                              Text(
-                                'Connect People\naround the world',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                            ),
+                            SizedBox(
+                              height: 15.0,
+                            ),
+                            Text(
+                              'Lorem ipsum dolor sit amet, consect adipiscing',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.bold,
                               ),
-                              SizedBox(
-                                height: 15.0,
-                              ),
-                              Text(
-                                'Lorem ipsum dolor sit amet, consect adipiscing',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 12.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(40.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Center(
-                                child: Image(
-                                  image:
-                                      AssetImage('assets/images/image_2.png'),
-                                  height: 300.0,
-                                  width: 300.0,
-                                ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                              child: Image(
+                                image: AssetImage('assets/images/image_2.png'),
+                                height: 300.0,
+                                width: 300.0,
                               ),
-                              SizedBox(
-                                height: 30.0,
+                            ),
+                            SizedBox(
+                              height: 30.0,
+                            ),
+                            Text(
+                              'Get a new experience\nof imagination',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
                               ),
-                              Text(
-                                'Get a new experience\nof imagination',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                            ),
+                            SizedBox(
+                              height: 15.0,
+                            ),
+                            Text(
+                              'Lorem ipsum dolor sit amet, consect adipiscing',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.bold,
                               ),
-                              SizedBox(
-                                height: 15.0,
-                              ),
-                              Text(
-                                'Lorem ipsum dolor sit amet, consect adipiscing',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 12.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(40.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Center(
-                                child: Image(
-                                  image:
-                                      AssetImage('assets/images/image_3.png'),
-                                  height: 300.0,
-                                  width: 300.0,
-                                ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                              child: Image(
+                                image: AssetImage('assets/images/image_3.png'),
+                                height: 300.0,
+                                width: 300.0,
                               ),
-                              SizedBox(
-                                height: 30.0,
+                            ),
+                            SizedBox(
+                              height: 30.0,
+                            ),
+                            Text(
+                              'Get a new experience\nof imagination',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
                               ),
-                              Text(
-                                'Get a new experience\nof imagination',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                            ),
+                            SizedBox(
+                              height: 15.0,
+                            ),
+                            Text(
+                              'Lorem ipsum dolor sit amet, consect adipiscing',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.bold,
                               ),
-                              SizedBox(
-                                height: 15.0,
-                              ),
-                              Text(
-                                'Lorem ipsum dolor sit amet, consect adipiscing',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 12.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: _buildPageIndicator(),
-                  )
-                ],
-              ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: _buildPageIndicator(),
+                ),
+                _currentPage != _numPages - 1
+                    ? Expanded(
+                        child: Align(
+                          alignment: FractionalOffset.bottomRight,
+                          child: TextButton(
+                            onPressed: () {
+                              _pageController.nextPage(
+                                  duration: Duration(
+                                    milliseconds: 500,
+                                  ),
+                                  curve: Curves.ease);
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  'Next',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22.0,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Icon(
+                                  Icons.arrow_forward,
+                                  color: Colors.white,
+                                  size: 30.0,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    : Text(''),
+              ],
             ),
           ),
         ),
       ),
+      bottomSheet: _currentPage == _numPages - 1
+          ? Container(
+              height: 100.0,
+              width: double.infinity,
+              color: Colors.white,
+              child: GestureDetector(
+                onTap: () {},
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 30),
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(
+                        color: Color(0xFF5B16D0),
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          : Text(''),
     );
   }
 }
